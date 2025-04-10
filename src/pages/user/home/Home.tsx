f24033488f95dce2e6d90567f582b17353ad4917
safi miazi <mohibullamiazi@gmail.com>
2025-04-10 22:28:32 +0600

    Add ProductMenu component to Home page for enhanced category display

--- src/pages/user/home/Home.tsx
+++ src/pages/user/home/Home.tsx
@@ -1,10 +1,12 @@
 import { Carousel } from '../../../components/common/Carousel';
 import OfferProductsSection from '../../../components/pages/Home/OfferProducts';
+import ProductMenu from '../../../components/pages/Home/ProductMenu';
 
 const Home = () => {
     return (
         <div>
             <Carousel/>
+            <ProductMenu/>
             <OfferProductsSection/>
         </div>
     );
